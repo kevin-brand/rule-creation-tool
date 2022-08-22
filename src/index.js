4167e4b4757eae527c1ae6e3e7fe0c22086bfeea
Kevin Brand <kevin-brand@outlook.com>
2022-08-22 15:34:55 +0200

    added ability to create new rules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,125 @@
 
-let rules = document.querySelector("#rule-list")
+let rulesList = document.querySelector("#rule-list")
+let addRuleButton = document.querySelector('#add-new-rule-button')
 
-new Sortable(rules, {
+new Sortable(rulesList, {
     handle: '.handle',
     animation: 200
 })
+
+addRuleButton.addEventListener('click', () => {
+    addRule();
+})
+
+function addRule() {
+    let listGroupItem = document.createElement('div')
+    listGroupItem.classList.add(('list-group-item'))
+
+    listGroupItem.appendChild(createHandle())
+    listGroupItem.appendChild(createRule())
+    listGroupItem.appendChild(createClose())
+
+    rulesList.appendChild(listGroupItem)
+}
+
+function createHandle() {
+    let handle = document.createElement('div')
+    handle.classList.add('handle')
+    handle.classList.add('flex-center')
+
+    let span = document.createElement('span')
+    span.classList.add('material-symbols-sharp')
+    span.appendChild(document.createTextNode('menu'))
+
+    handle.appendChild(span)
+
+    return handle
+}
+
+function createClose() {
+    let close = document.createElement('div')
+    close.classList.add('close')
+    close.classList.add('flex-center')
+
+    let span = document.createElement('span')
+    span.classList.add('material-symbols-sharp')
+    span.appendChild(document.createTextNode('close'))
+
+    close.appendChild(span)
+
+    return close
+}
+
+function createRule() {
+    let ruleDiv = document.createElement('div')
+    ruleDiv.classList.add('rule')
+    ruleDiv.classList.add('flex-center')
+
+    let ifP = createPElement('if')
+    let featureSelect = createSelectElement('feature', 'feature', ['TEST #1'])
+    //let isP = createPElement('is')
+    let comparisonSelect = createSelectElement(
+        'comparison', 
+        'comparison', 
+        ['GREATER', 'GREATER EQUAL', 'EQUAL', 'NOT EQUAL', 'LESS', 'LESS EQUAL']
+    )
+    let valueInputElement = createDataListInputElement('value', 'features', ['Test #1'])
+    let thenP = createPElement('then')
+    let magnitudeSelect = createSelectElement('magnitude', 'magnitude', ['WEAK', 'STRONG'])
+    let effectSelect = createSelectElement('effect', 'effect', ['POSITIVE', 'NEGATIVE'])
+    let effectP = createPElement('effect')
+
+    ruleDiv.appendChild(ifP)
+    ruleDiv.appendChild(featureSelect)
+    //ruleDiv.appendChild(isP)
+    ruleDiv.appendChild(comparisonSelect)
+    ruleDiv.appendChild(valueInputElement)
+    ruleDiv.appendChild(thenP)
+    ruleDiv.appendChild(magnitudeSelect)
+    ruleDiv.appendChild(effectSelect)
+    ruleDiv.appendChild(effectP)
+
+    return ruleDiv
+}
+
+function createPElement(text) {
+    let p = document.createElement('p')
+    p.appendChild(document.createTextNode(text))
+    return p
+}
+
+function createSelectElement(className, name, options) {
+    let select = document.createElement('select')
+    select.classList.add(className)
+    select.setAttribute('name', name)
+
+    options.forEach(option => {
+        let optionDOM = document.createElement('option')
+        optionDOM.appendChild(document.createTextNode(option))
+        select.appendChild(optionDOM)
+    });
+
+    return select;
+}
+
+function createDataListInputElement(className, listName, dataList) {
+    let input = document.createElement('input')
+    input.classList.add(className)
+    input.setAttribute('type', 'text')
+    input.setAttribute('list', listName)
+
+    if (document.getElementById(listName) == null) {
+        let list = document.createElement('datalist')
+        list.setAttribute('id', listName)
+
+        dataList.forEach(listElement => {
+            let optionDOM = document.createElement('option')
+            optionDOM.appendChild(document.createTextNode(listElement))
+            list.appendChild(optionDOM)
+        });
+
+        document.querySelector('body').appendChild(list)
+    }
+
+    return input;
+}
